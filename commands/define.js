b1c88862556ac430f9238d3cf859cc93f592614b
eleonore122 <40723433+eleonore122@users.noreply.github.com>
2020-12-02 10:26:23 -0600

    updated define command
    searching now works properly as far as i can tell. current code tries to find a "similar" match to user input in the list of keywords to check for typos, but if none are found it will search the definitions for a matching word, and kick back all definitions that contain the user input. if still no matches are found it will simply reply with "no matches found"

--- commands/define.js
+++ commands/define.js
@@ -15,7 +15,7 @@ module.exports = {
 
 		const keys = Object.keys(wordFile);
 
-		const inputString = args.join().toLowerCase();
+		const inputString = args.join(' ').toLowerCase();
 		matchFound = false;
 
 		let x;
@@ -24,7 +24,8 @@ module.exports = {
 			const tempVar = stringSimilarity.compareTwoStrings(`${inputString}`, `${keys[x]}`);
 			if (tempVar === 1) {
 				const word = keys.find(elem => elem === inputString);
-				const msg = Object.values(wordFile[word]);
+				const msgArray = Object.values(wordFile[word]);
+				const msg = msgArray.join('');
 
 				const embedMessage = new Discord.MessageEmbed()
 					.setColor('#ffff99')
@@ -44,56 +45,67 @@ module.exports = {
 
 			for (i = 0; i < keys.length; i++) {
 				const tempVar = stringSimilarity.compareTwoStrings(inputString, keys[i]);
+				const pageSize = pages.length;
 				if (tempVar > 0.6) {
 					const word = keys.find(elem => elem === keys[i]);
-					const msg = Object.values(wordFile[word]);
+					const msgArray = Object.values(wordFile[word]);
+					const msg = msgArray.join('');
 					pages.push(msg);
+					matchFound = true;
+				}
+				else if (pageSize === 0) {
+					for (i = 0; i < keys.length; i++) {
+						const definitions = Object.values(wordFile);
+						if (definitions[i].split(' ').includes(inputString)) {
+							const msg = definitions[i];
+							pages.push(msg);
+							matchFound = true;
+						}
+					}
 				}
 			}
-			const pageSize = pages.length;
+		}
 
-			if (pageSize === 0) {
-				message.channel.send('could not find any matches');
+		if (matchFound === false) {
+			message.channel.send('could not find any matches');
+		}
 
-			}
+		else{
+			const embedMessage = new Discord.MessageEmbed()
+				.setTitle('Did You Mean One Of These?')
+				.setColor('#ffff99')
+				.setFooter(`Page ${page} of ${pages.length}`)
+				.setDescription(pages[page - 1]);
 
-			else {
-				const embedMessage = new Discord.MessageEmbed()
-					.setTitle('Did You Mean One Of These?')
-					.setColor('#ffff99')
-					.setFooter(`Page ${page} of ${pages.length}`)
-					.setDescription(pages[page - 1]);
+			message.channel.send(embedMessage).then(msg => {
+				// eslint-disable-next-line no-unused-vars
+				msg.react('◀️').then(_r => {
+					msg.react('▶️');
 
-				message.channel.send(embedMessage).then(msg => {
-					// eslint-disable-next-line no-unused-vars
-					msg.react('◀️').then(_r => {
-						msg.react('▶️');
+					const backwardsFilter = (reaction, user) => reaction.emoji.name === '◀️' && user.id === message.author.id;
+					const forwardsFilter = (reaction, user) => reaction.emoji.name === '▶️' && user.id === message.author.id;
 
-						const backwardsFilter = (reaction, user) => reaction.emoji.name === '◀️' && user.id === message.author.id;
-						const forwardsFilter = (reaction, user) => reaction.emoji.name === '▶️' && user.id === message.author.id;
+					const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 });
+					const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 });
 
-						const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 });
-						const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 });
-
-						backwards.on('collect', (reaction, user) => {
-							if (page === 1) return;
-							page--;
-							embedMessage.setDescription(pages[page - 1]);
-							embedMessage.setFooter(`Page ${page} of ${pages.length}`);
-							reaction.users.remove(user);
-							msg.edit(embedMessage);
-						});
-						forwards.on('collect', (reaction, user) => {
-							if (page === pages.length) return;
-							page++;
-							embedMessage.setDescription(pages[page - 1]);
-							embedMessage.setFooter(`Page ${page} of ${pages.length}`);
-							reaction.users.remove(user);
-							msg.edit(embedMessage);
-						});
+					backwards.on('collect', (reaction, user) => {
+						if (page === 1) return;
+						page--;
+						embedMessage.setDescription(pages[page - 1]);
+						embedMessage.setFooter(`Page ${page} of ${pages.length}`);
+						reaction.users.remove(user);
+						msg.edit(embedMessage);
+					});
+					forwards.on('collect', (reaction, user) => {
+						if (page === pages.length) return;
+						page++;
+						embedMessage.setDescription(pages[page - 1]);
+						embedMessage.setFooter(`Page ${page} of ${pages.length}`);
+						reaction.users.remove(user);
+						msg.edit(embedMessage);
 					});
 				});
-			}
+			});
 		}
 	},
 };
