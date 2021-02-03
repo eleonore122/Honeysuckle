ea439e07ad6fe4c1b42c32263d0a19af2d1d0687
eleonore122 <40723433+eleonore122@users.noreply.github.com>
2021-02-02 18:59:19 -0600

    fixed spell checker bug
    i did a dumb, and nested a for loop that would skip the spell checker. that no longer happens

--- commands/define.js
+++ commands/define.js
@@ -3,6 +3,7 @@ const stringSimilarity = require('string-similarity');
 const Discord = require('discord.js');
 
 let pages = [];
+let pageSize = pages.length;
 let page = 1;
 let matchFound = false;
 
@@ -44,68 +45,80 @@ module.exports = {
 			page = 1;
 
 			for (i = 0; i < keys.length; i++) {
-				const tempVar = stringSimilarity.compareTwoStrings(inputString, keys[i]);
-				const pageSize = pages.length;
-				if (tempVar > 0.6) {
+				const tempVar = stringSimilarity.compareTwoStrings(`${inputString}`, `${keys[i]}`);
+				pageSize = pages.length;
+				if (tempVar > 0.65) {
 					const word = keys.find(elem => elem === keys[i]);
 					const msgArray = Object.values(wordFile[word]);
 					const msg = msgArray.join('');
 					pages.push(msg);
 					matchFound = true;
-				}
-				else if (pageSize === 0) {
-					for (i = 0; i < keys.length; i++) {
-						const definitions = Object.values(wordFile);
-						if (definitions[i].split(' ').includes(inputString)) {
-							const msg = definitions[i];
-							pages.push(msg);
-							matchFound = true;
-						}
-					}
+					const embedMessage = new Discord.MessageEmbed()
+						.setColor('#ffff99')
+						.setDescription(`${msg}`);
+
+					return message.channel.send(embedMessage);
 				}
 			}
 		}
 
-		if (matchFound === false) {
-			message.channel.send('could not find any matches');
-		}
+		if (pageSize === 0) {
+			let i;
+			pages = [];
+			page = 1;
 
-		else{
-			const embedMessage = new Discord.MessageEmbed()
-				.setTitle('Did You Mean One Of These?')
-				.setColor('#ffff99')
-				.setFooter(`Page ${page} of ${pages.length}`)
-				.setDescription(pages[page - 1]);
 
-			message.channel.send(embedMessage).then(msg => {
+			for (i = 0; i < keys.length; i++) {
+				const definitions = Object.values(wordFile);
+				if (definitions[i].split(' ').includes(inputString)) {
+					const msg = definitions[i];
+					pages.push(msg);
+					matchFound = true;
+				}
+			}
+
+
+			if (matchFound === false) {
+				message.channel.send('could not find any matches');
+			}
+
+			else{
+				const embedMessage = new Discord.MessageEmbed()
+					.setTitle('Did You Mean One Of These?')
+					.setColor('#ffff99')
+					.setFooter(`Page ${page} of ${pages.length}`)
+					.setDescription(pages[page - 1]);
+
+				message.channel.send(embedMessage).then(msg => {
 				// eslint-disable-next-line no-unused-vars
-				msg.react('◀️').then(_r => {
-					msg.react('▶️');
+					msg.react('◀️').then(_r => {
+						msg.react('▶️');
 
-					const backwardsFilter = (reaction, user) => reaction.emoji.name === '◀️' && user.id === message.author.id;
-					const forwardsFilter = (reaction, user) => reaction.emoji.name === '▶️' && user.id === message.author.id;
+						const backwardsFilter = (reaction, user) => reaction.emoji.name === '◀️' && user.id === message.author.id;
+						const forwardsFilter = (reaction, user) => reaction.emoji.name === '▶️' && user.id === message.author.id;
 
-					const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 });
-					const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 });
+						const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 });
+						const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 });
 
-					backwards.on('collect', (reaction, user) => {
-						if (page === 1) return;
-						page--;
-						embedMessage.setDescription(pages[page - 1]);
-						embedMessage.setFooter(`Page ${page} of ${pages.length}`);
-						reaction.users.remove(user);
-						msg.edit(embedMessage);
-					});
-					forwards.on('collect', (reaction, user) => {
-						if (page === pages.length) return;
-						page++;
-						embedMessage.setDescription(pages[page - 1]);
-						embedMessage.setFooter(`Page ${page} of ${pages.length}`);
-						reaction.users.remove(user);
-						msg.edit(embedMessage);
+						backwards.on('collect', (reaction, user) => {
+							if (page === 1) return;
+							page--;
+							embedMessage.setDescription(pages[page - 1]);
+							embedMessage.setFooter(`Page ${page} of ${pages.length}`);
+							reaction.users.remove(user);
+							msg.edit(embedMessage);
+						});
+						forwards.on('collect', (reaction, user) => {
+							if (page === pages.length) return;
+							page++;
+							embedMessage.setDescription(pages[page - 1]);
+							embedMessage.setFooter(`Page ${page} of ${pages.length}`);
+							reaction.users.remove(user);
+							msg.edit(embedMessage);
+						});
 					});
 				});
-			});
+			}
 		}
 	},
 };
--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,6 @@ module.exports = {
 	description: 'Ping!',
 	// eslint-disable-next-line no-unused-vars
 	execute(message, args) {
-		message.channel.send('Pong.');
+		message.channel.send(`${args}`);
 	},
 };
